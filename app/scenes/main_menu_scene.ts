618c2f64d2697d1394a7a78b49b932c9a34af330
Arthur Cohen <arthurcohen@live.com>
2019-10-29 20:17:02 -0300

    data modelling test
    TODO: Analisar viabilidade de testes unitários para aplicação

--- app/scenes/main_menu_scene.ts
+++ app/scenes/main_menu_scene.ts
@@ -1,5 +1,7 @@
 import { Application, Text } from 'pixi.js';
 import Scene from '../interfaces/scene';
+import { Result } from '../model/result';
+import { Sentence } from '../model/sentence';
 import SceneManager from '../scene_manager';
 import GameScene from './game_scene';
 
@@ -8,14 +10,14 @@ export default class MainMenuScene implements Scene {
 
   public render(container: PIXI.Container, app: Application) {
     const message: Text = new Text(
-            'Main Menu',
-            { fontFamily: 'Arial', fontSize: 32, fill: 'white' }
-        );
+      'Main Menu',
+      { fontFamily: 'Arial', fontSize: 32, fill: 'white' }
+    );
 
     const button: Text = new Text(
-            'Iniciar',
-            { fontFamily: 'Arial', fontSize: 42, fill: 'red', stroke: '#666AFFFF', strokeThickness: 1 }
-        );
+      'Iniciar',
+      { fontFamily: 'Arial', fontSize: 42, fill: 'red', stroke: '#666AFFFF', strokeThickness: 1 }
+    );
 
     button.interactive = true;
     button.buttonMode = true;
@@ -29,5 +31,33 @@ export default class MainMenuScene implements Scene {
 
   private onButtonClick(event: PIXI.interaction.InteractionEvent): any {
     SceneManager.setCurrentScene(new GameScene());
+
+    const sentence1 = new Sentence(
+      'O {} atropelou o {}',
+      ['carro', 'menino'],
+      {
+        'O carro atropelou o menino': new Result('O carro atropelou o menino', 'http://placekitten.com/200/300'),
+        'O menino atropelou o carro': new Result('O menino atropelou o carro', 'http://placekitten.com/300/200')
+      }
+    );
+
+    console.log(sentence1.resultFor(['carro', 'menino']));
+    console.log(sentence1.resultFor(['menino', 'carro']));
+
+    const sentence2 = new Sentence(
+      'O {} atropelou o {} com um {}',
+      ['carro', 'menino', 'skate'],
+      {
+        'O carro atropelou o menino com um skate': new Result('O carro atropelou o menino', 'http://placekitten.com/200/300'),
+        'O carro atropelou o skate com um menino': new Result('O carro atropelou o menino', 'http://placekitten.com/200/300'),
+        'O menino atropelou o carro com um skate': new Result('O menino atropelou o carro', 'http://placekitten.com/300/200'),
+        'O menino atropelou o skate com um carro': new Result('O menino atropelou o carro', 'http://placekitten.com/300/200'),
+        'O skate atropelou o menino com um carro': new Result('O menino atropelou o carro', 'http://placekitten.com/300/200'),
+        'O skate atropelou o carro com um menino': new Result('O menino atropelou o carro', 'http://placekitten.com/300/200'),
+      }
+    );
+
+    console.log(sentence2.resultFor(['carro', 'menino', 'skate']));
+    console.log(sentence2.resultFor(['menino', 'carro', 'skate']));
   }
 }
