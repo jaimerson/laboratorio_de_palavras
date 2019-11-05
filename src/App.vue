abd052767a266cf2b8753e2231f464d7aa565762
mrblack <daniel99955.04@gmail.com>
2019-11-05 19:59:36 -0300

    iniciando fluxo principal e tela do jogo

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,21 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-app ma-5 pa-5 class="background-home">
+    <router-view></router-view>
   </v-app>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld
-  },
   data: () => ({
     //
   })
 })
 </script>
+<style>
+  .background-home {
+    background-color: #B1CED5 !important;
+  }
+</style>
