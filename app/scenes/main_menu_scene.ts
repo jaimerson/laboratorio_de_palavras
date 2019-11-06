8f912d8174f23088c511894d3725b938874cb46f
babbujah <bczion@hotmail.com>
2019-11-05 21:18:46 -0300

    Alteração da tela de resultados com adição do atlas de texturas com PIXI.

--- app/scenes/main_menu_scene.ts
+++ app/scenes/main_menu_scene.ts
@@ -3,6 +3,7 @@ import Scene from '../interfaces/scene';
 import SentenceRepository from '../repositories/sentence_repository';
 import SceneManager from '../scene_manager';
 import GameScene from './game_scene';
+import ResultScene from './result_scene';
 
 export default class MainMenuScene implements Scene {
   public identifier: string = 'main_menu';
@@ -18,19 +19,38 @@ export default class MainMenuScene implements Scene {
       { fontFamily: 'Arial', fontSize: 42, fill: 'red', stroke: '#666AFFFF', strokeThickness: 1 }
     );
 
+    const buttonResultScreen: Text = new Text(
+      'Tela de Resultados',
+      { fontFamily: 'Arial', fontSize: 42, fill: 'red', stroke: '#666AFFFF', strokeThickness: 1 }
+    );
+
     button.interactive = true;
     button.buttonMode = true;
     button.on('pointerdown', this.onButtonClick);
+
+    buttonResultScreen.interactive = true;
+    buttonResultScreen.buttonMode = true;
+    buttonResultScreen.on('pointerdown', this.buttonClickResultScreen);
 
     message.position.set(app.view.width / 2 - message.width / 2, app.view.height / 2 - message.height / 2);
 
     button.position.set(message.position.x, message.position.y + 50);
 
-    [message, button].forEach((x) => { container.addChild(x); });
+    buttonResultScreen.position.set(button.position.x, button.position.y + 50);
+
+    [message, button, buttonResultScreen].forEach((x) => { container.addChild(x); });
+
+
+    
   }
 
   private onButtonClick(event: PIXI.interaction.InteractionEvent): any {
     SceneManager.setCurrentScene(new GameScene(SentenceRepository.randomSentence()));
   }
 
+
+  
+  private buttonClickResultScreen(event: PIXI.interaction.InteractionEvent): any {
+    SceneManager.setCurrentScene(new ResultScene(SentenceRepository.randomSentence()));
+  }
 }
--- app/scenes/result_scene.ts
+++ app/scenes/result_scene.ts
@@ -1,10 +1,38 @@
 import { Application } from 'pixi.js';
 import Scene from '../interfaces/scene';
+import { setupMaster } from 'cluster';
+import { Sentence } from '../model/sentence';
+
 
 export default class ResultScene implements Scene {
   public identifier: string = 'result';
 
+  constructor( public setence: Sentence ){
+    
+  }
+
   public render(container: PIXI.Container, app: Application) {
-    throw new Error('Method not implemented.');
+    //let Application = PIXI.Application
+    app = new Application({
+      //width: 512,
+      //height: 512,
+      antialias: true,
+      transparent: false,
+      resolution: 1,
+      backgroundColor: 0x58D3F7
+    });
+
+
+    let loader = PIXI.loader;
+	  loader
+      .add( "../assets/atlasLabWord.json" )
+      .load( setup );
+
+    //[message, button].forEach((x) => { container.addChild(x); });
+    //throw new Error('Method not implemented.');
+
+    function setup(){
+
+    }
   }
 }
