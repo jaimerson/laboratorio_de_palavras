efb728d6b90e134a34d5d2d9d9e1e2c3672575c6
Jaimerson Araújo <jaimersonaraujo@gmail.com>
2019-11-11 19:08:34 -0300

    wip

---
+++ spec/integration_spec.ts
@@ -0,0 +1,20 @@
+import { Sentence } from "../app/model/sentence";
+import SentenceRepository from "../app/repositories/sentence_repository";
+
+describe("Sentence loading to Result", () => {
+    beforeAll(() => {
+        SentenceRepository.loadFromFile();
+    });
+
+    it('loads sentences from yml file', () => {
+        const result = SentenceRepository.getSentences();
+        expect(result[0].base).toEqual("O {} correu atrás do {}");
+    });
+
+    it('calculates results', () => {
+        const sentence : Sentence = SentenceRepository.getSentences()[0];
+        expect(sentence.resultFor(['menino', 'cachorro']).title).toEqual('O menino correu atrás do cachorro');
+        expect(sentence.resultFor(['cachorro', 'menino']).title).toEqual('O cachorro correu atrás do menino');
+        expect(() => sentence.resultFor(['cachorro', 'gato']).title).toThrowError(/Undefined result/);
+    });
+});
