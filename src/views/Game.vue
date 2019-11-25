24a256ebeb9a80f0f22eaf0a7fbd7ced27998a6c
Ítalo Vieira <italogv@hotmail.com>
2019-11-25 09:32:45 -0300

    Padroniza página Game com a página de Resultados

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,13 +1,23 @@
 <template>
   <v-container fluid class="background-home">
-    <v-layout>
-      <v-row align="center" justify="center">
-        <v-layout  class="justify-center">
+    <v-row align="end" justify="start" class="ma-0">
+      <v-col cols="3">
+        <v-img
+          src="../../assets/clara.gif"
+          max-width="300"
+          max-height="400"
+          position="bottom"
+          contain="true"
+        />
+      </v-col>
+      <v-col cols="7">
+        <v-row>
           <v-card flat class="background-home">
             <v-img
               :src="imagePath"
               max-height="600"
               max-width="800"
+              contain="true"
               >
               <v-row
                 align="center"
@@ -18,7 +28,7 @@
                   <v-card-title class="display-1 ma-0">
                     {{fraseInicio}}
                   </v-card-title>
-                  <v-card-text class="display-1">
+                  <v-card-text class="display-1 text-center">
                     <div class="px-5 mx-5">
                       {{base}}
                     </div>
@@ -26,24 +36,30 @@
               </v-row>
             </v-img>
           </v-card>
-        </v-layout>
-      </v-row>
-    </v-layout>
-    <v-layout class="center" ma-5 pa-5 flex wrap justify-center>
-      <v-btn v-for='value in availableValues' :key='value' rounded @click="setValue(value)" :disabled="disabled(value)" color="cyan lighten-4" class='mr-2' v-html="value">
-      </v-btn>
-    </v-layout>
-    <v-layout flex wrap justify-start>
-      <v-btn rounded to="/" color="red lighten-3" class="mr-1">
-        Menu
-      </v-btn>
-      <v-btn rounded @click="reset" color="red lighten-3" class="mr-1">
-        Limpar
-      </v-btn>
-      <v-btn rounded @click="next" :disabled="!sentenceComplete" color="cyan lighten-4" class="mr-1">
+        </v-row>
+        <v-row class="ma-10">
+          <v-col>
+            <v-row align="center" justify="center">
+              <v-btn v-for='value in availableValues' :key='value' rounded @click="setValue(value)" :disabled="disabled(value)" color="cyan lighten-4" class='mr-2' v-html="value">
+              </v-btn>
+            </v-row>
+
+          </v-col>
+        </v-row>
+
+        <v-row align="center" justify="center">
+          <v-btn rounded to="/" color="yellow lighten-3" class="mr-1">
+            Menu
+          </v-btn>
+          <v-btn rounded @click="reset" color="red lighten-3" class="mr-1">
+            Limpar
+          </v-btn>
+          <v-btn rounded @click="next" :disabled="!sentenceComplete" color="yellow lighten-3" class="mr-1">
         Avançar
-      </v-btn>
-    </v-layout>
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
