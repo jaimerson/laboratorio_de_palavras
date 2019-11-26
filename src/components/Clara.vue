e6d7323c06ad9c05018b21d4c1e5af7b50c0ba4d
Ítalo Vieira <italogv@hotmail.com>
2019-11-26 20:45:54 -0300

    Melhorias tela game [WIP]

---
+++ src/components/Clara.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-flex xs5 offset-xs1>
+    <div>
+      <v-img src="../../assets/prateleira.png" height="100" width="300" ma-5></v-img>
+      <v-img src="../../assets/clara.png" :max-width="this.imageSize"></v-img>
+    </div>
+  </v-flex>
+</template>
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    props: ['imageSize']
+}
+</script>
